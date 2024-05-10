1070021d8f401450d823c6527a6079c6537be6f5
olita16 <119816415+olita16@users.noreply.github.com>
2024-05-10 19:57:09 +0200

    add redux, add styles

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
-import css from '../ContactForm/ContactForm.module.css';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { useSelector, useDispatch } from 'react-redux';
+import { getVisibleContacts } from '../../redux/selectors';
+import { addContact } from '../../redux/contactsSlice';
+import css from './ContactForm.module.css';
 
-const ContactFrom = ({ onSubmit }) => {
+const nameInputId = nanoid();
+const numberInputId = nanoid();
+
+const ContactForm = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
+  const contacts = useSelector(getVisibleContacts);
+  const dispatch = useDispatch();
+
   const handleSubmit = event => {
     event.preventDefault();
-    onSubmit({ name, number });
+
+    const isInContacts = contacts.some(
+      contact => contact.name.toLowerCase().trim() === name.toLowerCase().trim()
+    );
+
+    if (isInContacts) {
+      return Notify.info(`${name} is already in contacts`);
+    }
+
+    dispatch(addContact({ name, number }));
     setName('');
     setNumber('');
   };
 
-  const handleChange = ({ target }) => {
-    const { name, value } = target;
+  const handleChange = event => {
+    const { name, value } = event.target;
+
     switch (name) {
       case 'name':
         setName(value);
@@ -25,36 +46,41 @@ const ContactFrom = ({ onSubmit }) => {
         return;
     }
   };
-
   return (
-    <form className={css.contactForm} onSubmit={handleSubmit}>
-      <label className={css.formLabel}>
-        Name
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          value={name}
-          onChange={handleChange}
-        />
-      </label>
-      <label className={css.formLabel}>
-        Number
-        <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          value={number}
-          onChange={handleChange}
-        />
-      </label>
-      <button type="submit" className={css.addButton}>Add contact</button>
-    </form>
+    <div>
+      <form className={css.contactForm} onSubmit={handleSubmit}>
+        <label className={css.formLabel} htmlFor={nameInputId}>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        <label className={css.formLabel} htmlFor={numberInputId}>
+          Number
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleChange}
+            pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+            title="Phone number must be at least 10 digits long or follow the format +380931112233 or +38 093 333 4455."
+            required
+          />
+        </label>
+
+        <button className={css.addButton} type="submit">
+          Add contact{' '}
+        </button>
+      </form>
+    </div>
   );
 };
 
-export default ContactFrom;
+export default ContactForm;
